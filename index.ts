ff9adb5918499aa37fb881e21a43546359111e2e
KIMYUNMEE <jayoo8665@naver.com>
2022-06-23 22:34:59 +0900

    feat: Typescript    객체안의 속성이 변경 불가능하게 도와주는 readonly와 type alias

--- index.ts
+++ index.ts
@@ -201,3 +201,71 @@ console.log( 내함수(123) )
 //as 문법은 이럴 때 쓰자!
 //1)왜 타입에러가 나는지 정말 모르겠는 상황에 임시로 에러해결용으로 사용하거나
 //2)내가 어떤 타입이 들어올지 정말 확실하게 알고 있는데 컴파일러 에러가 방해할 때
+
+
+//7.readonly로 잠그기
+
+const 출생지역 = 'seoul'; 
+출생지역 = 'busan'; //const 변수는 여기서 에러남
+
+//const 변수는 값이 변하지 않는 변수를 만들고 싶을 때 const 쓰면 됩니다. 재할당시 오류남
+
+const 여친 = {
+  name : '엠버'
+}
+여친.name = '유라';  //const 변수지만 에러안남
+//하지만 object 자료를 const에 집어넣어도 object 내부는 마음대로 변경가능
+//const 변수는 재할당만 막아줄 뿐이지 그 안에 있는 object 속성 바꾸는 것 까지 관여하지 않기 때문에 내부는 변경 가능
+//readonly 키워드는 속성 왼쪽에 붙일 수 있으며 특정 속성을 변경불가능하게 잠궈줌
+
+type Girlfriend = {
+  readonly name : string,
+}
+
+let 여친 :Girlfriend = {
+  name : '엠버'
+}
+
+여친.name = '유라' //readonly라서 에러남
+//한번 부여된 후엔 앞으로 바뀌면 안될 속성들을 readonly로 잠궈보자
+//물론 readonly는 컴파일시 에러를 내는 것일 뿐 변환된 js 파일 보시면 잘 바뀌긴 한다.
+
+//만약 type 속성 몇개가 선택사항이라면 
+//ex) 어떤 object자료는 color, width 속성이 둘다 필요하지만 어떤 object 자료는 color 속성이 선택사항이라면 
+//type alias를 여러개 만들어야하는게 아니라 물음표연산자만 추가하면 됩니다.
+
+type Square = {
+  color? : string,
+  width : number,
+}
+
+let 네모2 :Square = { 
+  width : 100 
+}
+//Square라는 type alias를 적용한 object 자료를 하나 만들고 ?쓴덕에 color 속성이 없어도 에러가 나지 않는다.
+//물음표는 "undefined 라는 타입도 가질 수 있다~"라는 뜻
+
+//type 키워드 여러개를 합칠 수 있다.
+
+type Name = string;
+type Age = number;
+type NewOne = Name | Age; 
+//OR 연산자를 이용해서 Union type을 만들 수도 있다 => NewOne 타입에 마우스를 올리면 string | number로 보인다.
+
+
+type PositionX = { x: number };
+type PositionY = { y: number };
+type XandY = PositionX & PositionY
+let 좌표 :XandY = { x : 1, y : 2 }
+//object에 지정한 타입의 경우 합치기도 가능 & 기호를 쓴다면 object 안의 두개의 속성을 합쳐준다.
+//위 코드에서 XandY 타입은 { x : number, y : number } 이렇게 정의 되어있을 것이다.
+//Type alias & Type alias 만 가능한게 아니라 Type alias & { name : string } 이런 것도 가능하다.
+
+
+//type 키워드는 재정의가 불가능하다.
+
+type Name = string;
+type Name = number;
+
+//이렇게 하면 에러남 
+// interface 키워드를 배우게 될텐데 이 키워드를 쓰면 재정의가 가능
