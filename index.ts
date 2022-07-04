efabb1c66a9d3c0924200da5e357f4f7e63bf0cf
KIMYUNMEE <jayoo8665@naver.com>
2022-07-04 16:40:17 +0900

    :memo: Add 함수 rest 파라미터, destructuring 문법
    rest 파라미터, destructuring 문법 공부하기

--- index.ts
+++ index.ts
@@ -564,3 +564,76 @@ let 변수 :Dog & Animal = { name : '멍멍' }
 //(주의)그런데 name : string , name : number 라서 에러가 나는 것이지 둘다 name : string 타입이면 에러가 나지않고 하나로 합쳐줌 
 
 
+//14.함수에 어떤 파라미터가 몇개 들어올지 미리 정의가 불가능할 때 즉, 3개일지 4개일지 100개일지 모른다면 점3개 ...로 rest 파라미터를 만들어주면 됩니다.
+
+function 전부더하기(...a){
+  console.log(a)
+}
+
+전부더하기(1,2,3,4,5)
+//위와같이 함수 파라미터 작명할 때 점3개 붙여주면 여기엔 파라미터 잔뜩 들어올 수 있습니다~라고 정의할 수 있습니다.
+//이런걸 rest 파라미터라고 합니다.
+//rest특징 
+//1. rest 파라미터는 다른 일반 파라미터 뒤에만 올 수 있습니다. 2.rest 파라미터자리에 집어넣은 값들은 전부 [ ] 안에 담겨있습니다.
+
+// rest 파라미터 타입지정은
+
+function 전부더하기(...a :number[]){
+  console.log(a)
+}
+
+전부더하기(1,2,3,4,5)
+// rest 파라미터는 항상 [ ] 안에 담겨오기 때문에 타입지정도 array처럼 하면 됩니다.
+
+
+// Spread operator랑 형태는 비슷한데 다릅니다.array 혹은 object 괄호 벗기고 싶을 때 왼쪽에 사용
+
+let arr = [3,4,5];
+let arr2 = [1,2, ...arr]
+console.log(arr2)
+//위와같이 array 혹은 object 왼쪽에 점3개 붙이면 괄호 벗겨주세요~ 라는 뜻입니다. arr2 출력해보면 [1,2,3,4,5]가 나옵니다.
+// 종합하면 괄호벗겨주는 ...spread는 array, object 자료 왼쪽에, 여러개의 파라미터를 의미하는 ...rest는 함수선언할 때 소괄호 안에 출몰합니다. 
+
+
+// 15.Destructuring 문법 개념설명
+//:자바스크립트에서 array, object 안에 있는 데이터를 빼서 변수로 만들고 싶을 때 쓰는 문법입니다.
+
+let 사람 = { student : true, age : 20 }
+let student = 사람.student;
+let age = 사람.age
+//원래는 이렇게 작성하는데 새로운 문법이 발생했습니다. Destructuring 이라는 것인데 변수로 빠르고 쉽게 뺄 수 있도록 도와주는 문법입니다. 
+
+let { student123, age123 } = { student123 : true, age123 : 20 }
+// 이렇게 쓰면 똑같이 변수로 뺄 수 있습니다. 이걸 destructuring 문법이라고 합니다.
+
+let [a, b] = ['안녕', 100]
+//array 자료도 왼쪽오른쪽 똑같아보이게 변수 작성하면 변수로 쉽게 뺄 수 있습니다. 
+//단 array특징은 object destructuring할 땐 변수이름을 속성이름과 맞춰주는게 편리하고 array destructuring할 땐 변수이름 맘대로 작명가능합니다. 
+
+//15-2. Destructuring 문법도 함수 파라미터에 사용가능
+
+//변수만들 때 기존 object에 있던 자료를 파라미터로 집어넣고 싶으면 
+
+let person = { student : true, age : 20 }
+
+function 함수(a, b){
+  console.log(a, b)
+}
+함수(person.student, person.age)
+//기존에는  object에 있던걸 person.student 이렇게 각각 찝어서 집어넣으면 되는데 destructuring 문법을 사용하면 더 쉽게 사용 가능합니다.
+
+let person = { student : true, age : 20 }
+
+function 함수({student, age}){
+  console.log(student, age)
+}
+함수({ student : true, age : 20 })
+
+//파라미터 변수만들 때 { student, age }라고 쓰면 파라미터로 들어오는 { student : 어쩌구 }는 student 라는 변수에 저장해주세요
+//파라미터로 들어오는 { age : 어쩌구 }는 age 라는 변수에 저장해주세요~ 라는 뜻입니다.
+//object 자료니까 변수 작명할 때 object 속성명으로 유의해서 작성해야합니다.
+
+
+
+
+
